48c3bdba26042b04dc61a29c4bdbbf70b86a82e0
Sergey Teplyakov <Sergey.Teplyakov@gmail.com>
2017-09-27 20:48:17 -0700

    First version of a post about local functions.

---
+++ src/LocalFunctions/LocalFunctions/ImplicitClosure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LocalFunctions
+{
+    public class ImplicitClosure
+    {
+        public int ImplicitAllocation(int arg)
+        {
+            if (arg == int.MaxValue)
+            {
+                // This code is effectively unreachable
+                Func<int> a = () => arg;
+            }
+
+            int local = 42;
+            return Local();
+
+            int Local() => local;
+        }
+    }
+}
---
+++ src/LocalFunctions/LocalFunctions/RecursiveLocalFunction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LocalFunctions
+{
+    public class RecursiveLocalFunction
+    {
+public static List<Type> BaseTypesAndSelf2(Type type)
+{
+    Action<List<Type>, Type> addBaseType = null;
+    addBaseType = (lst, t) =>
+    {
+        lst.Add(t);
+        if (t.BaseType != null)
+        {
+            addBaseType(lst, t.BaseType);
+        }
+    };
+
+    var result = new List<Type>();
+    addBaseType(result, type);
+    return result;
+}
+
+public static List<Type> BaseTypesAndSelf(Type type)
+{
+    List<Type> AddBaseType(List<Type> lst, Type t)
+    {
+        lst.Add(t);
+        if (t.BaseType != null)
+        {
+            AddBaseType(lst, t.BaseType);
+        }
+        return lst;
+    }
+
+    return AddBaseType(new List<Type>(), type);
+}
+    }
+}
